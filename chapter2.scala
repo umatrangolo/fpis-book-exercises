03a48d73f092d2c9d0a59a27f9a5a20c0f3ce710
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-11-04 18:08:46 +0000

    Ex 2.2: Check if array is sorted given a predicate

--- chapter2.scala
+++ chapter2.scala
@@ -2,6 +2,7 @@ import scala.annotation.tailrec
 
 object chapter2 {
 
+  // Ex 2.1
   // 0 1 1 2 3 5 8 13 ...
   // Should be tail recursive
   @tailrec
@@ -9,5 +10,14 @@ object chapter2 {
     case 0 => 0
     case 1 => 1
     case _ => if (i == n) b else fib(n, i + 1, b, a + b)
+  }
+
+  // Ex 2.2
+  // Implement isSorted, which checks whether an Array[A] is sorted
+  // according to a given comparison function
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @tailrec
+    def scan(i: Int): Boolean = if (i == as.size - 1) true else if (ordered(as(i), as(i + 1))) scan(i + 1) else false
+    if (as.isEmpty) true else scan(0)
   }
 }
