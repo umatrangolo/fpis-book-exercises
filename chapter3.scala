33b4a354e20843577559bb1424c5fbd98e5b2288
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-11-05 09:13:49 +0000

    Ex 3.1: List + Pattern matching

---
+++ chapter3.scala
@@ -0,0 +1,20 @@
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): List[A] = if (as.isEmpty) Nil else Cons(as.head, apply(as.tail: _*))
+}
+
+// Ex 3.1
+// The result is 3 cause the matched case is the third one
