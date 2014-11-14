1c0d772bebd51228b7f990b1eb8ddb29499a3e00
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-11-14 17:35:34 +0000

    Ex 4.6: Either impl

--- chapter4.scala
+++ chapter4.scala
@@ -1,4 +1,4 @@
-import scala.{ Option => _ }
+import scala.{ Option => _, Either => _ }
 
 sealed trait Option[+A] {
   def map[B](f: A => B): Option[B]
@@ -69,4 +69,30 @@ object Chapter4 {
     case head :: tail => f(head).flatMap { res => traverse(tail)(f).map { res :: _ } }
   }
   def sequence2[A](a: List[Option[A]]): Option[List[A]] = traverse(a) { x => x }
+}
+
+
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B]
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B]
+  def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B]
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C]
+}
+
+case class Left[+E](value: E) extends Either[E, Nothing]{
+  override def map[B](f: Nothing => B): Either[E, B] = this
+  override def flatMap[EE >: E, B](f: Nothing => Either[EE, B]): Either[EE, B] = this
+  override def orElse[EE >: E,B >: Nothing](alternative: => Either[EE, B]): Either[EE, B] = alternative
+  override def map2[EE >: E, B, C](b: Either[EE, B])(f: (Nothing, B) => C): Either[EE, C] = this
+}
+
+case class Right[+A](value: A) extends Either[Nothing, A] {
+  override def map[B](f: A => B): Either[Nothing, B] = Right(f(value))
+  override def flatMap[EE >: Nothing, B](f: A => Either[EE, B]): Either[EE, B] = f(value)
+  override def orElse[EE >: Nothing,B >: A](b: => Either[EE, B]): Either[EE, B] = this
+  override def map2[EE >: Nothing, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = b.flatMap { r => Right(f(value, r)) }
+}
+
+object Either {
+  def Try[A](a: => A): Either[Exception, A] = try Right(a) catch { case e: Exception => Left(e) }
 }
