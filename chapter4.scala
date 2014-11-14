737a31742967438202c2ea8e10a009ba5e131b69
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-11-14 12:46:09 +0000

    Ex 4.3: map2 function

--- chapter4.scala
+++ chapter4.scala
@@ -33,4 +33,16 @@ object Chapter4 {
   def mean(xs: Seq[Double]): Option[Double] = if (xs.isEmpty) None else Some(xs.sum / xs.size)
   def variance(xs: Seq[Double]): Option[Double] = mean(xs).flatMap { m => mean(xs.map { x => math.pow(x - m, 2) }) }
 
+  def lift[A,B](f: A => B): Option[A] => Option[B] = _.map(f)
+
+  // Ex 4.3
+  // Write a generic function map2 that combines two Option(s)
+  // values using a binary function. If either Option value is None,
+  // then the return value is too.
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = for {
+    ra <- a
+    rb <- b
+  } yield {
+    f(ra, rb)
+  }
 }
