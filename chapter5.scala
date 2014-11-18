e0cabe838c2e2fad0c2d275a154ccf284666d8f3
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-11-18 23:59:21 +0000

    Ex 5.2: take and drop

--- chapter5.scala
+++ chapter5.scala
@@ -1,5 +1,3 @@
-import scala.{ Stream => _ }
-
 sealed trait Stream[+A] {
   def headOption: Option[A]
 
@@ -7,14 +5,29 @@ sealed trait Stream[+A] {
   // Write a function to convert a Stream to a List, which will force its
   // evaluation and let you look at it in the REPL.
   def toList: List[A]
+
+  // Ex 5.2
+  // Write the function take(n) for returning the first n elements of a
+  // Stream, and drop(n) for skipping the first n elements of a Stream.
+  def take(n: Int): Stream[A]
+  def drop(n: Int): Stream[A]
 }
+
 case object Empty extends Stream[Nothing] {
   override def headOption = None
   override def toList = Nil
+  override def take(n: Int) = Empty
+  override def drop(n: Int) = Empty
 }
+
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A] {
   override def headOption: Option[A] = Some(h())
   override def toList: List[A] = h() :: t().toList
+  override def take(n: Int): Stream[A] = if (n == 0) Stream.empty else this match {
+    case Cons(h, t) if n == 1 => Cons(h, () => Stream.empty)
+    case Cons(h, t) => Cons(h, () => t().take(n - 1))
+  }
+  override def drop(n: Int): Stream[A] = if (n == 0) this else t().drop(n - 1)
 }
 
 object Stream {
