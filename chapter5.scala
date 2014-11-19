82a0c7514304fe90cd5d88e8e245fb4c9bdbb2f6
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-11-19 14:28:29 +0000

    Ex 5.9: from stream func

--- chapter5.scala
+++ chapter5.scala
@@ -105,4 +105,8 @@ object Stream {
     tail
   }
 
+  // Ex 5.9
+  // Write a function that generates an infinite stream of integers,
+  // starting from n, then n + 1, n + 2, and so on.
+  def from(n: Int): Stream[Int] = Stream.cons(n, from(n + 1))
 }
