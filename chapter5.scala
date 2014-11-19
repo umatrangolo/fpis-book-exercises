e7e07028b8000243d3304a2d05e3ec92c0b6ac4b
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-11-19 15:17:49 +0000

    Ex 5.11: unfold

--- chapter5.scala
+++ chapter5.scala
@@ -117,4 +117,14 @@ object Stream {
     def _fib(a: Int, b: Int): Stream[Int] = Stream.cons(a, _fib(b, a + b))
     Stream.cons(0, _fib(0, 1))
   }
+
+  // Ex 5.11
+  // Write a more general stream-building function called unfold. It
+  // takes an initial state, and a function for producing both the next
+  // state and the next value in the generated stream.
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
+    case Some((h, s)) => Stream.cons(h, unfold(s)(f))
+    case None => Stream.empty
+  }
+  def fib2(): Stream[Int] = unfold((0, 1)) { z => Some((z._1, (z._2, z._1 + z._2))) }
 }
