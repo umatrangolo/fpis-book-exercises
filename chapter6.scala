e74970af78569bb5b2060919ffa1137a030cea5e
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-11-24 10:55:10 +0000

    Ex 6.2: double rng

--- chapter6.scala
+++ chapter6.scala
@@ -23,4 +23,14 @@ object Random {
     val (rnd, rng2) = rng.nextInt
     (scala.math.abs(rnd), rng2)
   }
+
+  // Ex 6.2
+  // Write a function to generate a Double between 0 and 1, not
+  // including 1. Note: You can use Int.MaxValue to obtain the maximum
+  // positive integer value, and you can use x.toDouble to convert an x:
+  // Int to a Double.
+  def double(rng: Rng): (Double, Rng) = {
+    val (nextNonNegativeInt, nextRng) = nonNegativeInt(rng)
+    if (nextNonNegativeInt == Int.MaxValue) double(nextRng) else (nextNonNegativeInt.toDouble / Int.MaxValue, nextRng)
+  }
 }
