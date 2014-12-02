a26b8d3687028f35235894f83fb487c07ec7efb1
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-12-02 09:27:38 +0000

    Ex 6.9: map and map2 in terms of flatMap

--- chapter6.scala
+++ chapter6.scala
@@ -124,4 +124,6 @@ object Random {
     }
   }
 
+  def map2[A, B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s) { a => unit(f(a)) }
+  def map22[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = flatMap(ra) { a => map(rb) { b => f(a, b) } }
 }
