d71cb318f676b922c908aaaedc936ddcba6bb66e
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-12-02 09:24:29 +0000

    Ex 6.8: flatMap

--- chapter6.scala
+++ chapter6.scala
@@ -110,4 +110,18 @@ object Random {
   }
   def ints2(count: Int)(rng: Rng): (List[Int], Rng) = sequence(List.fill[Rand[Int]](count){ rng => rng.nextInt })(rng)
 
+  // Ex 6.8
+  // Implement flatMap, and then use it to implement nonNegativeLessThan.
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+    val (a, rng2) = f(rng)
+    g(a)(rng2)
+  }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n-1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    }
+  }
+
 }
