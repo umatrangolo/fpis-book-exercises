9dc99646c8e5628c6b20220debfa451e0c7486b4
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-12-28 17:09:25 +0100

    Ex 10.2: optionMonoid

---
+++ chapter10.scala
@@ -0,0 +1,50 @@
+package fpis.monoids
+
+trait Monoid[A] {
+  def op(a1: A, a2: A): A
+  def zero: A
+}
+
+object Monoids {
+  val stringMonoid = new Monoid[String] {
+    override def op(a1: String, a2: String): String = a1 + a2
+    override def zero: String = ""
+  }
+
+  def listMonoid[A] = new Monoid[List[A]] {
+    override def op(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
+    override def zero: List[A] = Nil
+  }
+
+  // Ex 10.1
+  // Give Monoid instances for integer addition and multiplication as
+  // well as the Boolean operators.
+  val intAddition: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 + a2
+    override def zero: Int = 0
+  }
+
+  val intMultiplication: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 * a2
+    override def zero: Int = 1
+  }
+
+  val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 | a2
+    override def zero: Boolean = false
+  }
+
+  val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 & a2
+    override def zero: Boolean = true
+  }
+
+  // Ex 10.2
+  // Give a Monoid instance for combining Option values.
+  def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    def op(a1: Option[A], a2: Option[A]): Option[A] = a1.orElse(a2)
+    def zero: Option[A] = None
+  }
+
+
+}
