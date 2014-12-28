ed07c0db7bcbbe8d42e0533cd639300d3e72fa7e
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-12-28 17:28:02 +0100

    Ex 10.3: endoMonoid

--- chapter10.scala
+++ chapter10.scala
@@ -46,5 +46,14 @@ object Monoids {
     def zero: Option[A] = None
   }
 
+  // Ex 10.3
+  // A function having the same argument and return type is sometimes
+  // called an endofunction. Write a monoid for endofunctions.
 
+  // En endo function is a function that has domain == co-domain, that
+  // is the type of its argument is the same of its return type.
+  def endoMonoid[A]: Monoid[A => A] = new Monoid[A => A] {
+    override def op(a1: A => A, a2: A => A): A => A = a => a2(a1(a))
+    override def zero: A => A = a => a
+  }
 }
