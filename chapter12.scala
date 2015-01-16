30f57cefe4f2dbfaf223841d6caa5a505edac09a
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2015-01-16 17:12:35 +0000

    Ex 12.2: new applicative

--- chapter12.scala
+++ chapter12.scala
@@ -2,7 +2,7 @@ package fpis.applicative
 
 import fpis.monads._
 
-trait Appicative[F[_]] extends Functor[F] {
+trait OldApplicative[F[_]] extends Functor[F] {
   // primitive
   def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
   def unit[A](a: => A): F[A]
@@ -18,6 +18,20 @@ trait Appicative[F[_]] extends Functor[F] {
   def sequence[A](fas: List[F[A]]): F[List[A]] = traverse(fas) { fa => fa }
   def replicateM[A](n: Int, fa: F[A]): F[List[A]] = sequence((0 to n).toList.map { i => fa })
   def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] = map2(fa, fb) { (a, b) => (a, b) }
+}
 
+trait Applicative[F[_]] extends Functor[F] {
 
+  // Ex 12.2
+  // The name applicative comes from the fact that we can formulate the
+  // Applicative interface using an alternate set of primitives, unit and
+  // the function apply, rather than unit and map2. Show that this
+  // formulation is equivalent in expressiveness by defining map2 and map
+  // in terms of unit and apply. Also establish that apply can be imple-
+  // mented in terms of map2 and unit.
+  def apply[A, B](fab: F[A => B])(fa: F[A]): F[B]
+  def unit[A](a: => A): F[A]
+
+  def map[A, B](fa: F[A])(f: A => B): F[B] = apply(unit(f))(fa)
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = apply(map(fa) { f.curried })(fb)
 }
